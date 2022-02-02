22fa1ac8bc9d497f5b74299117348edbcf35d841
Siti Rosita <srosita421@gmail.com>
2022-02-02 21:44:43 +0700

    Menambahkan Authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,9 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
     use Authenticatable, Authorizable, HasFactory;
 
@@ -31,12 +32,22 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
         'password',
     ];
 
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
     public function getJWTIdentifier()
     {
         return $this->getKey();
     }
 
-    public function getJWTCustomClass()
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
     {
         return [];
     }
--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,12 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
-$router->get('users', 'UserController@index');
-$router->post('/users', 'UserController@store');
-$router->get('/users/{id}', 'UserController@show');
-$router->post('/users/{id}/update', 'UserController@update');
-$router->delete('/users/{id}', 'UserController@destroy');
+
+    $router->get('users', 'UserController@index');
+    $router->post('/users', 'UserController@store');
+    $router->get('/users/{id}', 'UserController@show');
+    $router->put('/users/{id}/update', 'UserController@update');
+    $router->delete('/users/{id}', 'UserController@destroy');
 
 $router->group(['prefix' => 'auth'], function () use ($router) {
     $router->post('/register', 'AuthController@register');
